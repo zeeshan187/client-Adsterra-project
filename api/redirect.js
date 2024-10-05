650eaacf7fdeb353e3f07473dd994f7ad2e116a1
Zeeshan <mzeeshi187@gmail.com>
2024-10-05 15:43:30 -0700

    initial commit

--- api/redirect.js
+++ api/redirect.js
@@ -1,9 +1,11 @@
 export default function handler(req, res) {
-    const adsterraLink = 'https://capefast.com/wwha61gfm9?key=a893b2f960ec7abf692a83d0582ddbe8';
-    
+  const adsterraLink = 'https://capefast.com/wwha61gfm9?key=a893b2f960ec7abf692a83d0582ddbe8';
+
+  // Wait for 1 second before redirecting
+  setTimeout(() => {
     res.writeHead(302, {
       Location: adsterraLink,
     });
     res.end();
-  }
-  
+  }, 1000);
+}
